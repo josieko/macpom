ea81870016c533f5bb31a00df4b6eb02ceb63fcc
josieko <josipakukor@gmail.com>
2024-08-08 20:10:23 +0200

    Rename single story

--- src/component/Buttons--task-buttons.stories.tsx
+++ src/component/Buttons--task-buttons.stories.tsx
@@ -2,3 +2,5 @@ import type { Story } from "@ladle/react";
 import TaskList from "./TaskList";
 
 export const TaskListSection: Story = () => <TaskList isActive={false} />;
+TaskListSection.storyName = "Add task";
+// <name>.storyName = "<new story name>" ---> name changing syntax
